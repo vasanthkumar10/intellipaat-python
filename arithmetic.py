3f1db437724a4afcf1f38a7320d14bd2ccacbeb9
unknown <vasizebron10@gmail.com>
2022-12-21 19:38:10 +0530

    python notes

---
+++ arithmetic.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def get_sum(n1, n2):
+    return n1 + n2
+
+def get_sub(n1, n2):
+    return n1 - n2
+
+def get_mul(n1, n2):
+    return n1 * n2
+
+def get_rem(n1, n2):
+    return n1 % n2
+
+
+def get_employee():
+    return 'SELECT * from {table_name}'
+
---
+++ queries.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def get_employee():
+    return 'SELECT * from {table_name}'
+
